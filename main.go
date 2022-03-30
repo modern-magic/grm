1abf818222d2d4ace24e1d34fb93686fae4e78b5
kanno <812137533@qq.com>
2022-03-30 18:09:20 +0800

    style: clean code

--- main.go
+++ main.go
@@ -23,14 +23,7 @@ Commands:
   current                                 Show current registry name
   use <registry>                          Change registry to registry
   add <registry> <url> [home]             Add one custom registry
-  set-auth [options] <registry> [value]   Set authorize information for a custom registry with a base64 encoded string or username and pasword
-  set-email <registry> <value>            Set email for a custom registry
-  set-hosted-repo <registry> <value>      Set hosted npm repository for a custom registry to publish packages
   del <registry>                          Delete one custom registry
-  home <registry> [browser]               Open the homepage of registry with optional browser
-  publish [options] [<tarball>|<folder>]  Publish package to current registry if current registry is a custom registry.
-   if you're not using custom registry, this command will run npm publish directly
-  test [registry]                         Show response time for specific or all registries
   help                                    Print this help
 `
 
